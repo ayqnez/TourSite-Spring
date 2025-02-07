007f1bdc66c4001e3ed15483a02021975b7167a8
ayqnez <www.ayanpro@gmail.com>
2025-02-07 22:20:53 +0500

    update uix8 and add some functions

---
+++ src/main/java/kz/kassen/Tour_App/repo/UserRepository.java
@@ -0,0 +1,10 @@
+package kz.kassen.Tour_App.repo;
+
+import kz.kassen.Tour_App.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+    Optional<UserModel> findByUsername(String username);
+}
